ae03ab7e89d5deae55e96e85f0dabf708ca970a8
Flash11552 <sfrovli7@gmail.com>
2024-12-02 23:12:05 +0400

    Update helper.py

--- BrandrdXMusic/utils/stuffs/helper.py
+++ BrandrdXMusic/utils/stuffs/helper.py
@@ -1,26 +1,29 @@
 # helper for strings
 
-class Helper(object):
-    HELP_M = '''KATEQORİYANIZI SEÇİN  
+class Helper:
+    HELP_M = """KATEQORİYANIZI SEÇİN  
 Hansı mövzuda köməyə ehtiyacınız varsa, dəstək söhbətində suallarınızı verin.
 
 Bütün əmrlər belə istifadə oluna bilər: /
-'''
-    HELP_ChatGPT = '''ChatGPT
+"""
+
+    HELP_ChatGPT = """ChatGPT
 
 ChatGPT ƏMRLƏRİ:
 
 /de ➠ Süni Zəka modelinə suallarınızı verib cavab alın.
-'''
-    HELP_Reel = '''Reel
+"""
+
+    HELP_Reel = """Reel
 
 Reel ƏMRLƏRİ:
 
 /ig [URL] ➠ Instagram videolarını yükləyin. Komandanın ardından Instagram URL-ni qeyd edin.
 /instagram [URL] ➠ Instagram videolarını yükləyin. URL-ni qeyd edin.
 /reel [URL] ➠ Instagram videolarını yükləyin. URL-ni qeyd edin.
-'''
-    HELP_TagAll = '''Tag
+"""
+
+    HELP_TagAll = """Tag
 
 Tag ƏMRLƏRİ:
 
@@ -40,15 +43,17 @@ Dayandırmaq üçün: /azstop
 
 ๏ /stag ➛ Təsadüfi şeir tagı  
 Dayandırmaq üçün: /sstop
-'''
-    HELP_Info = '''Məlumat
+"""
+
+    HELP_Info = """Məlumat
 
 Məlumat ƏMRLƏRİ:
 
 /id : Mövcud qrupun ID-sini alın. Mesaja cavab olaraq istifadə edildikdə həmin istifadəçinin ID-sini göstərir.  
 /info : İstifadəçi haqqında məlumat alın.
-'''
-    HELP_History = '''Tarixçə
+"""
+
+    HELP_History = """Tarixçə
 
 Tarixçə ƏMRLƏRİ:
 
@@ -67,22 +72,25 @@ Nümunələr:
 ⦿ /sg istifadəçi_adı  
 ⦿ /sg istifadəçi_ID  
 ⦿ /sg [mesaja cavab ver]
-'''
-    HELP_Couples = '''Cütlüklər
+"""
+
+    HELP_Couples = """Cütlüklər
 
 Cütlüklər ƏMRLƏRİ:
 
 /couples - 2 istifadəçi seçin və onların adlarını cütlük kimi göstərin.
-'''
-    HELP_Extra = '''Əlavə
+"""
+
+    HELP_Extra = """Əlavə
 
 Əlavə ƏMRLƏR:
 
 ⦿ /tgm ➠ Fotoşəkili (5 MB-dən kiçik) yükləyib link təqdim edir.  
 ⦿ /paste ➠ Mətn hissəsini yükləyib link təqdim edir.  
 ⦿ /tr ➠ Mətn tərcümə edir.
-'''
-    HELP_Action = '''Fəaliyyət
+"""
+
+    HELP_Action = """Fəaliyyət
 
 Fəaliyyət ƏMRLƏRİ:
 
@@ -99,9 +107,9 @@ Yalnız adminlər üçün:
  ❍ /mute <istifadəçi adı>: İstifadəçini səssiz edir.  
  ❍ /tmute <istifadəçi adı> x(m/s/g): İstifadəçini müəyyən müddətə səssiz edir.  
  ❍ /unmute <istifadəçi adı>: Səssizlik rejimini aradan qaldırır.
+"""
 
-'''
-    HELP_Search = '''Axtarış
+    HELP_Search = """Axtarış
 
 Axtarış ƏMRLƏRİ:
 
@@ -110,20 +118,23 @@ Axtarış ƏMRLƏRİ:
 
 Nümunə:  
 /google pyrogram: Ən yaxşı 5 nəticəni qaytarır.
-'''
-    HELP_Font = '''Şrift
+"""
+
+    HELP_Font = """Şrift
 
 Şrift modulu ilə mətnlərinizin şriftini dəyişə bilərsiniz.
 
 Əmr:  
 ◌ /font [Mətn]
-'''
-    HELP_Bots = '''Botlar
+"""
+
+    HELP_Bots = """Botlar
 
 Botlar haqqında məlumat almaq üçün əmrlər:  
 ◌ /bots - Qrupdakı botların siyahısını göstərir.
-'''
-    HELP_TG = '''Ⓣ-Qraf
+"""
+
+    HELP_TG = """Ⓣ-Qraf
 
 Ⓣ-Qraf ƏMRLƏRİ:
 
@@ -131,35 +142,41 @@ Telegraph linki ilə media yaradın!
 
 ◌ /tgm [istənilən media ilə cavab verin]  
 ◌ /tgt [istənilən media ilə cavab verin]
-'''
-    HELP_TD = '''Düzlük və Cəsarət
+"""
+
+    HELP_TD = """Düzlük və Cəsarət
 
 ƏMRLƏR:  
 
 ◌ /truth : Təsadüfi bir sual təqdim edir.  
 ◌ /dare : Təsadüfi bir tapşırıq təqdim edir.
-'''
-    HELP_Quiz = '''Viktorina
+"""
+
+    HELP_Quiz = """Viktorina
 
 Viktorina ƏMRİ:
 
 ◌ /quiz - Təsadüfi sual əldə etmək üçün.
-'''
-    HELP_TTS = '''Mətnə Səs
+"""
+
+    HELP_TTS = """Mətnə Səs
 
 ƏMR:
 
 ❀ Mətn Səsləndirmə  
 ◌ /tts : [mətn]  
-'''
-    HELP_Radio = '''Radio
+"""
+
+    HELP_Radio = """Radio
 
 Radio ƏMRİ:
 
 ◌ /radio - Səsli söhbətdə radio oynatmaq üçün.
-'''
-    HELP_Q = '''Sitat Yarat
+"""
+
+    HELP_Q = """Sitat Yarat
 
 ƏMR:
 
 ◌ /q : Mesajdan sitat yaradın.
+"""
