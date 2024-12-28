a641d13b3fb479a803698191948204843e656bd5
Flash11552 <sfrovli7@gmail.com>
2024-12-28 13:15:27 +0400

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_video("https://files.catbox.moe/gyln7f.MP4")
+            await message.reply_video("https://files.catbox.moe/09k6fh.mp4")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
