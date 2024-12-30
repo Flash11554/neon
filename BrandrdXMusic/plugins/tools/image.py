81763cd8c8ae8e2a2465d6c1251669fe377f5869
Flash11552 <sfrovli7@gmail.com>
2024-12-30 13:11:41 +0400

    Update image.py

--- BrandrdXMusic/plugins/tools/image.py
+++ BrandrdXMusic/plugins/tools/image.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from re import findall
+import re
 from bing_image_downloader import downloader
 from pyrogram import Client, filters
 from pyrogram.types import InputMediaPhoto, Message
@@ -15,7 +15,8 @@ async def google_img_search(client: Client, message: Message):
     except IndexError:
         return await message.reply("Şəkilləri hazır etmək üçün istədiyinizi yazın!")
 
-    lim = findall(r"lim=\d+", query)
+    query = re.sub(r"[^\w\s]", "", query).strip()  # Sanitize query
+    lim = re.findall(r"lim=\d+", query)
     try:
         lim = int(lim[0].replace("lim=", ""))
         query = query.replace(f"lim={lim}", "")
@@ -27,27 +28,23 @@ async def google_img_search(client: Client, message: Message):
     try:
         downloader.download(query, limit=lim, output_dir=download_dir, adult_filter_off=True, force_replace=False, timeout=60)
         images_dir = os.path.join(download_dir, query)
-        if not os.listdir(images_dir):
+        if not os.path.exists(images_dir) or not os.listdir(images_dir):
             raise Exception("No images were downloaded.")
-        lst = [os.path.join(images_dir, img) for img in os.listdir(images_dir)][:lim]  # Ensure we only take the number of images specified by lim
+        lst = [os.path.join(images_dir, img) for img in os.listdir(images_dir)][:lim]
     except Exception as e:
         return await message.reply(f"Şəkilləri göndərərkən səhv baş verdi: {e}")
 
     msg = await message.reply("𝔹𝕣𝕒𝕟𝕕𝕖𝕕𝕏𝕄𝕒𝕟𝕒𝕘𝕖𝕞𝕖𝕟𝕥 Scrapping images...")
 
-    count = 0
-    for img in lst:
-        count += 1
-        await msg.edit(f"=> Effect şəkilləri hazırlayır {count}")
+    for i, img in enumerate(lst, start=1):
+        await msg.edit(f"=> Effect şəkilləri hazırlayır {i}/{lim}")
 
     try:
-        await app.send_media_group(
-            chat_id=chat_id,
-            media=[InputMediaPhoto(media=img) for img in lst],
-            reply_to_message_id=message.id
-        )
-        shutil.rmtree(images_dir)
-        await msg.delete()
+        media = [InputMediaPhoto(media=img) for img in lst]
+        await app.send_media_group(chat_id=chat_id, media=media, reply_to_message_id=message.id)
     except Exception as e:
-        await msg.delete()
         return await message.reply(f"Şəkilləri göndərərkən səhv baş verdi: {e}")
+    finally:
+        if os.path.exists(images_dir):
+            shutil.rmtree(images_dir)
+        await msg.delete()
